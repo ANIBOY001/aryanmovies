5fcb67e2e1d095454086fce84c14a1a8844693de
ANIBOY001 <aniboy001@gmail.com>
2026-03-30 03:45:30 +0600

    Debug: Fix next.config.js for GitHub Pages, remove leftover file

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'export',
   distDir: 'dist',
+  basePath: '/aryanmovies',
+  assetPrefix: '/aryanmovies',
   images: {
     domains: ['image.tmdb.org', 'wsrv.nl', 'www.cineby.sc'],
     unoptimized: true,
