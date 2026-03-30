2add06effc3b89bea92473499ef31280f79f92ff
ANIBOY001 <aniboy001@gmail.com>
2026-03-30 16:25:17 +0600

    Fix Vercel deployment - use env var for basePath

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'export',
   distDir: 'dist',
-  basePath: '/aryanmovies',
+  basePath: process.env.GITHUB_PAGES === 'true' ? '/aryanmovies' : '',
   images: {
     domains: ['image.tmdb.org', 'wsrv.nl', 'www.cineby.sc'],
     unoptimized: true,
